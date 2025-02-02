7bfe50d73d3048f2402012b50ea3f1c7a49ab1b8
watanabeshoma <watanabe.shoma.2022@gmail.com>
2025-02-02 23:22:50 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow(): void {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('https://www.example.com'); // 最初に表示するページ
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
